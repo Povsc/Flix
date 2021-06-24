7704aa08d53808497d29a026b6b2e6c3fa11900b
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-06-24 16:33:54 -0400

    Added release date and rtaings

--- Flix/View Controllers/DetailsViewController.m
+++ Flix/View Controllers/DetailsViewController.m
@@ -14,7 +14,10 @@
 @property (weak, nonatomic) IBOutlet UIImageView *posterView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *blurImage;
+@property (weak, nonatomic) IBOutlet UIView *ratingView;
+@property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
 @end
 
 @implementation DetailsViewController
@@ -39,9 +42,32 @@
     self.titleLabel.text = self.movie[@"title"];
     self.navigationBar.title = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
+    self.dateLabel.text = self.movie[@"release_date"];
+    self.ratingLabel.text = [NSString stringWithFormat:@"%.1f", [self.movie[@"vote_average"] doubleValue]];//[self.movie[@"vote_average"] stringValue];
+
+//
+//    NSString *preText = @"RELEASED: ";
+//    NSString *postText = self.movie[@"release_date"];
+//    self.dateLabel.text = [preText stringByAppendingString:postText];
     
     [self.titleLabel sizeToFit];
     [self.synopsisLabel sizeToFit];
+    NSInteger displacement = self.synopsisLabel.frame.size.height;
+    
+    CGRect dateFrame = self.dateLabel.frame;
+    dateFrame.origin.y += displacement + 20;
+    self.dateLabel.frame = dateFrame;
+    
+    CGRect ratingFrame = self.ratingView.frame;
+    ratingFrame.origin.y += displacement;
+    self.ratingView.frame = ratingFrame;
+    
+    self.ratingView.layer.cornerRadius = 5;
+    self.ratingView.layer.masksToBounds = true;
+    float rating = [self.ratingLabel.text floatValue] / 10.0;
+    
+    [self.ratingView setBackgroundColor:[UIColor
+                                       colorWithHue:0.3f * rating saturation:1.0f brightness:0.7f alpha:1.0f]];
     
 // Code with some help from StackOverflow below:
 
